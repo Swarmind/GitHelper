ab8e844a035b07d389656d333a3398a653246727
jackbekket <jack_out_the_b0x@proton.me>
2024-09-26 18:50:42 +0400

    auth as an app

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/jackbekket/GitHelper
 go 1.23.1
 
 require (
-	github.com/google/go-github/v45 v45.2.0 // indirect
+	github.com/bradleyfalzon/ghinstallation/v2 v2.11.0
+	github.com/google/go-github/v65 v65.0.0
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
+	github.com/golang-jwt/jwt/v4 v4.5.0 // indirect
+	github.com/google/go-github/v62 v62.0.0 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
+	"time"
 
-	"github.com/google/go-github/v45/github"
+	ghinstallation "github.com/bradleyfalzon/ghinstallation/v2"
+	"github.com/google/go-github/v65/github"
+	"github.com/joho/godotenv"
 )
 
 // Define your API endpoint for handling webhook requests.
@@ -15,6 +21,18 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
 	var repoOwner *string
 	var repoName *string
+
+	// creating github client from private key
+	_ = godotenv.Load()
+	_id := os.Getenv("APP_ID")
+	//wh_secret := os.Getenv("WEBHOOK_SECRET")
+	pk_path := os.Getenv("PRIVATE_KEY_PATH")
+	app_id, err := strconv.Atoi(_id)
+	if err != nil {
+		// ... handle error
+		panic(err)
+	}
+	client := createClient(pk_path, app_id)
 
 	// Extract the issue event details from the webhook payload.
 	// ... (Logic to handle webhook payload and extract issue content)
@@ -56,7 +74,7 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		// ... (Rest of your logic to extract issue content and call 3rd service)
 		response := "hardcoded response"
 
-		respond(w, r, *repoOwner, *repoName, *issueID, response)
+		respond(w, r, client, *repoOwner, *repoName, *issueID, response)
 
 	default:
 		fmt.Fprintln(w, "Unknown event type received")
@@ -75,14 +93,15 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 
 }
 
-func respond(w http.ResponseWriter, r *http.Request, owner string, repo string, id int64, response string) {
+func respond(w http.ResponseWriter, r *http.Request, client *github.Client, owner string, repo string, id int64, response string) {
 	ctx := r.Context()
 
 	// Craft a reply message from the response from the 3rd service.
 	replyMessage := fmt.Sprintf("Here's the response from our 3rd service:\n%s", response)
 
 	// Create a new comment on the issue using the GitHub API.
-	client := github.NewClient(nil)
+
+	//client := github.NewClient(nil)
 	_, _, err := client.Issues.CreateComment(ctx, owner, repo, int(id), &github.IssueComment{
 		// Configure the comment with the issue's ID and other necessary details.
 		Body: &replyMessage,
@@ -96,6 +115,63 @@ func respond(w http.ResponseWriter, r *http.Request, owner string, repo string, 
 
 }
 
+func createClient(key_path string, app_id int) *github.Client {
+	const gitHost = "https://git.api.com"
+
+	privatePem, err := os.ReadFile(key_path)
+	if err != nil {
+		log.Fatalf("failed to read pem: %v", err)
+	}
+
+	itr, err := ghinstallation.NewAppsTransport(http.DefaultTransport, int64(app_id), privatePem)
+	if err != nil {
+		log.Fatalf("failed to create app transport: %v\n", err)
+	}
+	itr.BaseURL = gitHost
+
+	//create git client with app transport
+	client, err := github.NewClient(
+		&http.Client{
+			Transport: itr,
+			Timeout:   time.Second * 30,
+		},
+	).WithEnterpriseURLs(gitHost, gitHost)
+	//)
+
+	if err != nil {
+		log.Fatalf("failed to create git client for app: %v\n", err)
+	}
+
+	installations, _, err := client.Apps.ListInstallations(context.Background(), &github.ListOptions{})
+	if err != nil {
+		log.Fatalf("failed to list installations: %v\n", err)
+	}
+
+	//capture our installationId for our app
+	//we need this for the access token
+	var installID int64
+	for _, val := range installations {
+		installID = val.GetID()
+	}
+
+	token, _, err := client.Apps.CreateInstallationToken(
+		context.Background(),
+		installID,
+		&github.InstallationTokenOptions{})
+	if err != nil {
+		log.Fatalf("failed to create installation token: %v\n", err)
+	}
+
+	apiClient, err := github.NewClient(nil).WithAuthToken(
+		token.GetToken(),
+	).WithEnterpriseURLs(gitHost, gitHost)
+	if err != nil {
+		log.Fatalf("failed to create new git client with token: %v\n", err)
+	}
+
+	return apiClient
+}
+
 // Example implementation of your 3rd service call.
 func callThirdService(content string) (string, error) {
 	// Implement the logic to call your 3rd service here.
@@ -105,6 +181,7 @@ func callThirdService(content string) (string, error) {
 }
 
 func main() {
+
 	// ... (Set up your webhook endpoint and start the server)
 	http.HandleFunc("/webhook", handleWebhook)
 	log.Fatal(http.ListenAndServe(":8080", nil))
