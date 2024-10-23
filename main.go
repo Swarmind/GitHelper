eff603bdbb972ada2bec275c8d90518c122eb01c
JackBekket <jackatwork@yandex.ru>
2024-10-23 18:28:50 +0400

    better logging

--- main.go
+++ main.go
@@ -186,8 +186,9 @@ func createClients(app_id int) (error)  {
 	}
 
 	// log installations
-	log.Println("installations: ", installations)
+	//log.Println("installations: ", installations)
 	for _, installation := range installations {
+		log.Println("installation : ", installation)
 		log.Println("repository selection: ", installation.RepositorySelection)
 	}
 
