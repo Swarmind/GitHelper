a14a518264b0e97bf7ad5f695f665ff3af1717a4
JackBekket <jackatwork@yandex.ru>
2024-10-23 18:23:58 +0400

    print repo selection

--- main.go
+++ main.go
@@ -187,6 +187,9 @@ func createClients(app_id int) (error)  {
 
 	// log installations
 	log.Println("installations: ", installations)
+	for _, installation := range installations {
+		log.Println("repository selection: ", installation.RepositorySelection)
+	}
 
 
 	//capture our installationId for our app
