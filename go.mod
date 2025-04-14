00d9bd3f58125f05d53efdd2aecedd8a38ff8f90
JackBekket <jackatwork@yandex.ru>
2025-04-14 18:26:13 +0200

    fix types

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/JackBekket/GitHelper
 go 1.24.1
 
 require (
-	github.com/JackBekket/langgraphgo v0.1.2
+	github.com/JackBekket/langgraphgo v0.1.3
 	github.com/JackBekket/reflexia v0.1.2
 	github.com/bradleyfalzon/ghinstallation/v2 v2.11.0
 	github.com/go-git/go-git/v5 v5.12.0
--- pkg/agent/code_monkey/planner.go
+++ pkg/agent/code_monkey/planner.go
@@ -1,10 +1,5 @@
 package code_monekey
 
-import (
-	"fmt"
-	"regexp"
-)
-
 type Plan struct {
     Task string
     Steps []string
@@ -19,18 +14,3 @@ type ReWOO struct {
     Result string
 }
 
-func getPlan(task string) Plan {
-    planString := fmt.Sprintf("Plan: %s\n", task)
-    regexPattern := regexp.MustCompile(`Plan:\s*(.+)\s*(#E\d+)\s*=\s*(\w+)\s*\[([^\]]+)\]`)
-    planString = planString + "#E1 = step1 [some content]\n"
-    planString = planString + "#E2 = step2 [some content]\n"
-    planString = planString + "#E3 = step3 [some content]\n"
-    planString = planString + "#E4 = step4 [some content]\n"
-    planString = planString + "#E5 = step5 [some content]\n"
-    matches := regexPattern.FindAllStringSubmatch(planString, -1)
-    steps := make([]string, len(matches))
-    for i, match := range matches {
-        steps[i] = match[2]
-    }
-    return Plan{Task: task, Steps: steps, PlanString: planString}
-}
