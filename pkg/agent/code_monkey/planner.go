803226677c7fb83e9d60bdf2aedfece050a4b3d0
JackBekket <jackatwork@yandex.ru>
2025-04-14 18:33:30 +0200

    some comments

--- pkg/agent/code_monkey/planner.go
+++ pkg/agent/code_monkey/planner.go
@@ -1,5 +1,11 @@
 package code_monekey
 
+import (
+	"context"
+
+	graph "github.com/JackBekket/langgraphgo/graph/stategraph"
+)
+
 type Plan struct {
     Task string
     Steps []string
@@ -14,3 +20,13 @@ type ReWOO struct {
     Result string
 }
 
+// get_plan - функция для работы со State
+func get_plan(ctx context.Context, state graph.State) graph.State {
+    task := state.Get("task").(string)
+    result := planner.invoke(map[string]string{"task": task})
+    matches := re.findall(regex_pattern, result.content)
+    return ReWOO{
+        "steps": matches,
+        "plan_string": result.content,
+    }
+}
